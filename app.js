cf714d9211c8e404e77d9bc22f91be19940a66a0
muhammadasadprogramar <muhammadasad852233@gmail.com>
2024-05-27 17:53:05 +0500

    html mcq

---
+++ app.js
@@ -0,0 +1,190 @@
+// Q No 1
+
+let qNo1 = prompt(
+  "Who is the developer of HTML?\n🟢 Robert Cailliau\n🟢 Tim Thompson\n🟢 Charles Darwin\n🟢 Tim Berners-Lee"
+);
+let points = 0;
+
+if (qNo1 == "Tim Berners-Lee") {
+  document.write("Qno 1 ✅ Correct");
+  ++points;
+} else {
+  document.write("Qno 1 ❌ Wrong");
+  points;
+}
+
+document.write(" Your Point is " + points);
+
+document.write("<br>");
+
+// Q No 2
+
+let qNo2 = prompt(
+  "HTMl was first proposed in the year?\n🟢 1985\n🟢 1990\n🟢 1995\n🟢 2000"
+);
+let points1 = 0;
+
+if (qNo2 == 1990) {
+  document.write("Qno 2 ✅ Correct");
+  ++points1;
+} else {
+  document.write("Qno 2 ❌ Wrong");
+  points1;
+}
+
+document.write(" Your Point is " + points1);
+
+document.write("<br>");
+
+// Q No 3
+
+let qNo3 = prompt(
+  "What type of language is HTML?\n🟢 Scripting Language\n🟢 Markup Language\n🟢 Network Protocol\n🟢 Programing Language"
+);
+let points3 = 0;
+
+if (qNo3 == "Markup Language") {
+  document.write("Qno 3 ✅ Correct");
+  ++points3;
+} else {
+  document.write("Qno 3 ❌ Wrong");
+  points3;
+}
+
+document.write(" Your Point is " + points3);
+
+document.write("<br>");
+
+// Q No 4
+
+let qNo4 = prompt(
+  "What does the <br> tag add to your webpage?\n🟢 Paragraph break\n🟢 Long break\n🟢 Line break\n🟢 None of these"
+);
+let points4 = 0;
+
+if (qNo4 == "Line break") {
+  document.write("Qno 4 ✅ Correct");
+  ++points4;
+} else {
+  document.write("Qno 4 ❌ Wrong");
+  points4;
+}
+
+document.write(" Your Point is " + points4);
+
+document.write("<br>");
+
+// Q No 5
+
+let qNo5 = prompt("HTML is subset of?\n🟢 SGMD\n🟢 SGMH\n🟢 SGML\n🟢 None");
+let points5 = 0;
+
+if (qNo5 == "SGML") {
+  document.write("Qno 5 ✅ Correct");
+  ++points5;
+} else {
+  document.write("Qno 5 ❌ Wrong");
+  points5;
+}
+
+document.write(" Your Point is " + points5);
+
+document.write("<br>");
+
+// Q No 6
+
+let qNo6 = prompt(
+  "The body tag is usually used after?\n🟢 html tag\n🟢 title tag\n🟢 head tag\n🟢 em tag"
+);
+let points6 = 0;
+
+if (qNo6 == "head tag") {
+  document.write("Qno 6 ✅ Correct");
+  ++points6;
+} else {
+  document.write("Qno 6 ❌ Wrong");
+  points6;
+}
+
+document.write(" Your Point is " + points6);
+
+document.write("<br>");
+
+// Q No 7
+
+let qNo7 = prompt(
+  "Which character is used to represent the closing of a tag in HTML?\n🟢 *\n🟢 @\n🟢 /\n🟢 !"
+);
+let points7 = 0;
+
+if (qNo7 == "/") {
+  document.write("Qno 7 ✅ Correct");
+  ++points7;
+} else {
+  document.write("Qno 7 ❌ Wrong");
+  points7;
+}
+
+document.write(" Your Point is " + points7);
+
+document.write("<br>");
+
+// Q No 8
+
+let qNo8 = prompt(
+  "In HTML, tags that include both on and off tags are called?\n🟢 document tag\n🟢 comment tag\n🟢 container tag\n🟢 None of the above"
+);
+let points8 = 0;
+
+if (qNo8 == "container tag") {
+  document.write("Qno 8 ✅ Correct");
+  ++points8;
+} else {
+  document.write("Qno 8 ❌ Wrong");
+  points8;
+}
+
+document.write(" Your Point is " + points8);
+
+document.write("<br>");
+
+// Q No 9
+
+let qNo9 = prompt(
+  "Full form of URl is?\n🟢 Uniform read locator\n🟢 Uniform research locator\n🟢 Uniform resource locator\n🟢 United research locator"
+);
+let points9 = 0;
+
+if (qNo9 == "Uniform resource locator") {
+  document.write("Qno 9 ✅ Correct");
+  ++points9;
+} else {
+  document.write("Qno 9 ❌ Wrong");
+  points9;
+}
+
+document.write(" Your Point is " + points9);
+
+document.write("<br>");
+
+// Q No 8
+
+let qNo10 = prompt(
+  "Which program do you need to write HTMl?\n🟢 HTML development suite 4\n🟢 A graphics program\n🟢 Any text editor\n🟢 All of these"
+);
+let points10 = 0;
+
+if (qNo10 == "Any text editor") {
+  document.write("Qno 10 ✅ Correct");
+  ++points10;
+} else {
+  document.write("Qno 10 ❌ Wrong");
+  points10;
+}
+
+document.write(" Your Point is " + points10);
+
+// document.write("<br>");
+// document.write("<br>");
+
+// document.write("Total Marks is " + points + points1 + points3 + points4 + points5 + points6 + points7 + points8 + points9 + points10)
